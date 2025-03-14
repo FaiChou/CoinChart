56d310ddf23c27816e40ea56dc342eee0d522208
FaiChou <faichou.zh@gmail.com>
2025-03-14 20:15:51 +0800

    Initial Commit

---
+++ CoinChart/CoinChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinChartApp.swift
+//  CoinChart
+//
+//  Created by 周辉 on 2025/3/14.
+//
+
+import SwiftUI
+
+@main
+struct CoinChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinChart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinChart
+//
+//  Created by 周辉 on 2025/3/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
